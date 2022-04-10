a6d20c7bcc07e344f122560779287b7676a1474e
22ru <22ru@users.noreply.github.com>
2022-04-10 15:41:22 -0700

    dsdas

--- webring.js
+++ webring.js
@@ -2,7 +2,7 @@
 
 var siteList = [
     ["hat", "https://hat.neocities.org/"],
-    ["pompon", "https://pompon.neocities.org/"]
+    ["pompon", "https://pompon.neocities.org/"],
     ["youtube", "https://www.youtube.com/"],
     ["volksusa", "https://volksusastore.com/webstores/dollfie/index.php"]
 ];
